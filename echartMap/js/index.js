946fdaacf1445d3de09d3b15d447a7c4232e64c4
yexuan <414741043@qq.com>
2017-07-21 14:37:24 +0800

    add two demos
    1.echartMap;
    2.useBaiduApiToGetXY.

---
+++ echartMap/js/index.js
@@ -0,0 +1,52 @@
+var itemStyleInactive = {
+	normal : {
+		label : {
+			show : true,
+			textStyle : {
+				color : "#000",
+				fontSize : 16
+			}
+		},
+		areaColor : "transparent",
+		borderColor : "#c3e3fa",
+		borderWidth : 1
+	},
+	emphasis : {
+		label : {
+			show : true,
+			textStyle : {
+				color : "#000",
+				fontSize : 16
+			}
+		},
+		areaColor : "transparent",
+		borderColor : "#c3e3fa",
+		borderWidth : 1
+	}
+}
+var itemStyleActive = {
+	normal : {
+		label : {
+			show : true,
+			textStyle : {
+				color : "#ffffff",
+				fontSize : 14
+			}
+		},
+		areaColor : "#f5eed1",
+		borderColor : "#ffe979",
+		borderWidth : 1
+	},
+	emphasis : {
+		label : {
+			show : true,
+			textStyle : {
+				color : "#ffffff",
+				fontSize : 14
+			}
+		},
+		areaColor : "#f5eed1",
+		borderColor : "#ffe979",
+		borderWidth : 1
+	}
+}
