121f4f07596e6efb3ef811f873e8b4f069d2a0fd
yexuan <414741043@qq.com>
2017-09-13 16:41:44 +0800

    update api-portal

--- api-portal/js/index.js
+++ api-portal/js/index.js
@@ -3,4 +3,11 @@ $(function(){
 		$(this).toggleClass('active');
 		$('#js-menu-pop').slideToggle();
 	});
+	$(document).on('click',function(e){
+		var $target = $(e.target);
+		if($target.closest('#js-menu-pop').length === 0 && $target.closest('#js-menu-document').length === 0){
+			$('#js-menu-document').removeClass('active');
+			$('#js-menu-pop').slideUp();
+		}
+	});
 });
